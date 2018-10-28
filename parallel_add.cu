7a210529fbb3ca06ee80cec30c3f8d99cde9874a
Apollo1840 <zoucongyu1993@hotmail.com>
2018-10-28 23:42:34 +0200

    CUDA

---
+++ parallel_add.cu
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, float *x, float *y)
+{
+  int index = threadIdx.x;
+  int stride = blockDim.x;
+  for (int i = index; i < n; i += stride)
+      y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<20; // 1M elements
+  float *x, *y;
+
+  // Allocate Unified Memory 
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // 256 Threads
+  add<<<1, 256>>>(N, x, y);
+
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  
+  return 0;
+}
